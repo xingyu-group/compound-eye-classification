0b408bd0554f4f6cf54ccc939f2183a48686888a
Hao Xuan <lovettxh@gmail.com>
2024-06-10 18:08:25 -0600

    update

--- dataset.py
+++ dataset.py
@@ -12,59 +12,81 @@ def pil_loader(path):
 class CompoundEyeDataset(data.Dataset):
     def __init__(self, data_root=None, loader=pil_loader, num_img_per_folder=7400, train=True, transforms=None):
         self.train = train
-        self.seed = 0
+        self.seed = 14
         self.data_root = data_root
         self.loader = loader
         self.num_img_per_folder = num_img_per_folder
         self.fog_strength_split = 10
         self.transforms = transforms
         self.reset_random_state()
-        
+        self.random_split_data()
 
     def reset_random_state(self):
         self.rand_state = np.random.RandomState(self.seed)
     
     def random_split_data(self):
         image_list = []
+        labels_list = []
+        label = 0
         for i in ['A','E','I','O','U']:
+            temp_image_list = []
             if i in ['A','E','U']:
                 folder_num = self.rand_state.choice(8, 4)
             else:
                 folder_num = [0, 1, 2, 3]
             for j in folder_num:
-                tmp = glob.glob(os.path.join(self.data_root, 'Compound-eye-classification', i, str(j+1), '*.png'))
+                tmp = glob.glob(os.path.join(self.data_root, 'Compound-eye-classification', i, i+str(j+1), '*.png'))
                 if len(tmp) < self.num_img_per_folder:
-                    image_list += tmp
-                image_list += tmp[:self.num_img_per_folder]
-        self.rand_state.shuffle(image_list)
-        train_list = image_list[:int(len(image_list)*0.9)]
-        test_list = image_list[int(len(image_list)*0.9):]
-
-        self.train_list = train_list
-        self.test_list = test_list
+                    temp_image_list += tmp
+                temp_image_list += tmp[:self.num_img_per_folder]
+            labels_list += [label]*len(temp_image_list)
+            image_list += temp_image_list
+            label += 1
+        idx = self.rand_state.permutation(len(image_list))
+        image_list = np.array(image_list)
+        labels_list = np.array(labels_list)
+        train_list = image_list[idx[:int(len(idx)*0.9)]]
+        train_labels = labels_list[idx[:int(len(idx)*0.9)]]
+        test_list = image_list[idx[int(len(idx)*0.9):]]
+        test_labels = labels_list[idx[int(len(idx)*0.9):]]
         
         if self.train:
             self.imgs = train_list
+            self.labels = train_labels
         else:
-            self.test_list_fog_split = [[] for _ in range(self.fog_strength_split)]
-            for i in test_list:
-                img_num = int(i[5:9])-1
-                self.test_list_fog_split[img_num//(self.num_img_per_folder//self.fog_strength_split)].append(i)
+            self.test_data_fog_split = [[] for _ in range(self.fog_strength_split)]
+            self.test_label_fog_split = [[] for _ in range(self.fog_strength_split)]
+            for i in range(len(test_list)):
+                img_num = int(test_list[i][-10:-4])-1
+                fog_strength = img_num//(self.num_img_per_folder//self.fog_strength_split)
+                if fog_strength == self.fog_strength_split:
+                    fog_strength -= 1
+                self.test_data_fog_split[fog_strength].append(test_list[i])
+                self.test_label_fog_split[fog_strength].append(test_labels[i])
             self.imgs = test_list
+            self.labels = test_labels
     
     def set_test_fog_strength(self, fog_strength):
         assert fog_strength <= self.fog_strength_split and fog_strength > 0
-        self.imgs = self.test_list_fog_split[fog_strength-1]
+        self.imgs = self.test_data_fog_split[fog_strength-1]
+        self.labels = self.test_label_fog_split[fog_strength-1]
+        self.imgs = np.array(self.imgs)
+        self.labels = np.array(self.labels)
 
     def reset_test_fog_strength(self):
         self.imgs = []
-        for i in range(len(self.test_list_fog_split)):
-            self.imgs += self.test_list_fog_split[i]
-
+        self.labels = []
+        for i in range(len(self.test_data_fog_split)):
+            self.imgs += self.test_data_fog_split[i]
+            self.labels += self.test_label_fog_split[i]
+        self.imgs = np.array(self.imgs)
+        self.labels = np.array(self.labels)
+        
     def __getitem__(self, index):
         path = self.imgs[index]
         img = self.transforms(self.loader(path))
-        return img
+        label = self.labels[index]
+        return img, label
 
     def __len__(self):
         return len(self.imgs)
--- main.py
+++ main.py
@@ -1,12 +1,75 @@
+import os
 import torch
+import torch.nn as nn
 import torchvision
 from dataset import CompoundEyeDataset
+from utils.progress_bar import progress_bar
+from torchvision.utils import save_image
 
+os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+seed = 42
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
 
+def get_dataloader():
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.CenterCrop((800, 800)),
+        torchvision.transforms.ToTensor(),
+    ])
+    trainset = CompoundEyeDataset(data_root='../data', train=True, transforms=transform)
+    testset = CompoundEyeDataset(data_root='../data', train=False, transforms=transform)
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=12, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=12, shuffle=False)
+    return train_loader, test_loader
+
+def eval_dataset(model, data_loader, device, f=None):
+    model.eval()
+    err = 0
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(data_loader):
+            data, target = data.to(device), target.to(device)
+            logit = model(data)
+            err += (logit.data.max(1)[1] != target.data).float().sum()
+        print('accuracy: {:.2f}'.format((1-err/len(data_loader.dataset)) * 100), file=f, flush=True)
+
+def train(model, train_loader, optimizer):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = nn.CrossEntropyLoss()(output, target)
+        loss.backward()
+        optimizer.step()
+        progress_bar(batch_idx, len(train_loader))
+
+    
 def main():
-    trainset = CompoundEyeDataset(data_root='../data', train=True)
-    testset = CompoundEyeDataset(data_root='../data', train=False)
+    train_loader, test_loader = get_dataloader()
+    model = torchvision.models.resnet18(num_classes=5).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    schedular = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10)
+    # model = torchvision.models.wide_resnet101_2()
 
-    print(trainset.test_list == testset.test_list)
+    for epoch in range(10):
+        print(f'Epoch: {epoch}')
+        train(model, train_loader, optimizer)
+        schedular.step()
+        eval_dataset(model, train_loader, device)
+        eval_dataset(model, test_loader, device)
+
+    torch.save(model.state_dict(), './model-checkpoint/resnet50.pt')
+
+    # for data, target in test_loader:
+    #     save_image(data[5], 'test.png')
+    #     print(target[5])
+    #     break
+
 if __name__ == '__main__':
     main()
