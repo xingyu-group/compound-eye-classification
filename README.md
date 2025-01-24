c5e15229761d3f20ac15f1304a36823386b47bec
Hao Xuan <lovettxh@gmail.com>
2025-01-23 17:03:18 -0700

    update

---
+++ README.md
@@ -0,0 +1,28 @@
+# Compound Eye Classification
+
+This project focuses on the classification of compound eye images using machine learning techniques.
+
+## Installation
+To get started, clone the repository and install the required dependencies:
+```bash
+git clone https://github.com/yourusername/compound-eye-classification.git
+cd compound-eye-classification
+pip install -r requirements.txt
+```
+
+## Usage
+To train the model, run:
+```bash
+python train.py --data_path /path/to/dataset
+```
+
+
+
+## Model
+The model is built using a convolutional neural network (CNN) architecture. Details of the model architecture can be found in `model.py`.
+
+## Results
+The results of the model training and evaluation will be saved in the `results/` directory. This includes accuracy, loss plots, and confusion matrices.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+import argparse
 import torch
 import torch.nn as nn
 import torchvision
 from dataset import CompoundEyeDataset
 from utils.progress_bar import progress_bar
-from torchvision.utils import save_image
-import random
-import wandb
+
+parser = argparse.ArgumentParser(description='Compound Eye Classification Experiment') 
+parser.add_argument('--data_dir', default='../data',type=str)
+args = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
@@ -18,32 +20,35 @@ torch.backends.cudnn.deterministic = True
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 f = None
-wandb.init(project="test", entity="lovettxh"
-    , name="test"
-    # ,mode='disabled'
-    )
+
+if not os.path.exists('./model-checkpoint'):
+    os.makedirs('./model-checkpoint')
 
 test_loss_all = [[] for _ in range(10)]
 test_accuracy_all = [[] for _ in range(10)]
 
+# get dataloader for training and testing
+# training and testing data are randomly split with a ratio of 9:1
 def get_dataloader():
     transform = torchvision.transforms.Compose([
         torchvision.transforms.CenterCrop((800, 800)),
         torchvision.transforms.ToTensor(),
     ])
-    trainset = CompoundEyeDataset(data_root='../data', train=True, transforms=transform)
-    testset = CompoundEyeDataset(data_root='../data', train=False, transforms=transform)
+    trainset = CompoundEyeDataset(data_root=args.data_dir, train=True, transforms=transform)
+    testset = CompoundEyeDataset(data_root=args.data_dir, train=False, transforms=transform)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=12, shuffle=True)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=12, shuffle=False)
     return train_loader, test_loader
 
+# evaluate model accuracy on different fog strength 
+# images have been manually divided into 10 different fog strength
 def eval_diff_fog_strength(model, device):
     model.eval()
     transform = torchvision.transforms.Compose([
         torchvision.transforms.CenterCrop((800, 800)),
         torchvision.transforms.ToTensor(),
     ])
-    testset = CompoundEyeDataset(data_root='../data', train=False, transforms=transform)
+    testset = CompoundEyeDataset(data_root=args.data_dir, train=False, transforms=transform)
     with torch.no_grad():
         for s in range(10):
             testset.set_test_fog_strength(s+1)
@@ -57,9 +62,9 @@ def eval_diff_fog_strength(model, device):
                 err += (logit.data.max(1)[1] != target.data).float().sum()
             test_loss_all[s].append(loss/len(test_loader))
             test_accuracy_all[s].append((1-err/len(test_loader.dataset)) * 100)
-            # wandb.log({'test loss': loss/len(test_loader), 'test accuracy': (1-err/len(test_loader.dataset)) * 100})
             # print('accuracy: {:.2f}'.format((1-err/len(test_loader.dataset)) * 100), file=f, flush=True)
 
+# model evaluation on training/testing dataset
 def eval_dataset(model, data_loader, device):
     model.eval()
     err = 0
@@ -70,6 +75,7 @@ def eval_dataset(model, data_loader, device):
             err += (logit.data.max(1)[1] != target.data).float().sum()
         print('accuracy: {:.2f}'.format((1-err/len(data_loader.dataset)) * 100), file=f, flush=True)
 
+# model training process
 def train(model, train_loader, optimizer):
     model.train()
     loss_sum = 0
@@ -84,8 +90,9 @@ def train(model, train_loader, optimizer):
         progress_bar(batch_idx, len(train_loader), 
                     #  'Loss: {:.4f}'.format(loss.item())
                      )
+        
+        # fog strength evaluation is done every 1000 batches
         if (batch_idx+1) % 1000 == 0:
-            wandb.log({'train loss': loss_sum/1000})
             loss_sum = 0
             eval_diff_fog_strength(model, device)
             model.train()
@@ -93,10 +100,10 @@ def train(model, train_loader, optimizer):
     
 def main():
     train_loader, test_loader = get_dataloader()
+    # ResNet18 is selected as model backbone
     model = torchvision.models.resnet18(num_classes=5).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     schedular = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10)
-    # model = torchvision.models.wide_resnet101_2()
 
     for epoch in range(2):
         print(f'Epoch: {epoch}')
@@ -104,23 +111,9 @@ def main():
         schedular.step()
         eval_dataset(model, train_loader, device)
         eval_dataset(model, test_loader, device)
-    wandb.log({"test loss": wandb.plot.line_series(xs=range(len(test_loss_all[0])),
-                                                   ys=test_loss_all, 
-                                                   keys=["strength{}".format(i) for i in range(1, 11)],
-                                                   title="test loss",
-                                                   xname="fog strength")})
-    
-    wandb.log({"test accuracy": wandb.plot.line_series(xs=range(len(test_accuracy_all[0])),
-                                                   ys=test_accuracy_all, 
-                                                   keys=["strength{}".format(i) for i in range(1, 11)],
-                                                   title="test accuracy",
-                                                   xname="fog strength")})
-    # torch.save(model.state_dict(), './model-checkpoint/resnet18.pt')
 
-    # for data, target in test_loader:
-    #     save_image(data[5], 'test.png')
-    #     print(target[5])
-    #     break
+    torch.save(model.state_dict(), './model-checkpoint/resnet18.pt')
+
 
 if __name__ == '__main__':
     main()
